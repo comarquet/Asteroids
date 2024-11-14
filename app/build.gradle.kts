5396032df3f374480efc1e9a595723b39f4cec16
Corentin MARQUET <cormarquet@gmail.com>
2024-11-14 22:45:47 +0100

    Modifies metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+group = "com.example"
+version = "1.0.0"
+
 plugins {
     application
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
@@ -32,4 +36,12 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
-version = "1.0.0"
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            "Main-Class" to "com.example.AsteroidsApplication"
+        )
+    }
+}
+
+
